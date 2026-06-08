a1a9c712788bf1de025bc4944ccf8feb799dfe5d
yuria12 <yr.work12@gmail.com>
2026-06-08 10:28:24 +0900

    Issue-0-initial-frontend-setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
