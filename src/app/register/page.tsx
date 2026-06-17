e647404a84896e65101e65df179f5a66370088c8
yuria12 <yr.work12@gmail.com>
2026-06-17 15:42:07 +0900

    会員登録フォームの表示を調整

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -42,7 +42,7 @@ export default async function RegisterPage({ searchParams }: RegisterPageProps) 
         ]}
       />
 
-      <div className="auth-panel">
+      <div className="auth-panel auth-panel--register">
         <h1>会員登録</h1>
 
         <form className="auth-form auth-form--register" action={registerAction}>
@@ -52,48 +52,69 @@ export default async function RegisterPage({ searchParams }: RegisterPageProps) 
           <TextInput
             autoComplete="name"
             id="name"
-            label="お名前："
+            label="名前"
             name="name"
+            placeholder="例）山田 太郎"
+            required
           />
           <TextInput
             autoComplete="email"
             id="email"
-            label="メールアドレス："
+            label="メールアドレス"
             name="email"
+            placeholder="例）example@mail.com"
+            required
             type="email"
           />
-          <TextInput
-            autoComplete="postal-code"
-            id="postal-code"
-            label="郵便番号："
-            name="postalCode"
-            placeholder="123-4567"
-          />
+          <div className="auth-postal">
+            <TextInput
+              autoComplete="postal-code"
+              id="postal-code"
+              label="郵便番号"
+              name="postalCode"
+              placeholder="例）123-4567"
+              required
+            />
+            <button type="button">住所検索</button>
+          </div>
+          <p className="auth-form__note">
+            郵便番号を入力後、住所検索で住所を入力できます
+          </p>
           <TextInput
             autoComplete="street-address"
             id="address"
-            label="住所："
+            label="住所"
             name="address"
+            placeholder="例）東京都渋谷区神宮前1-2-3"
+            required
           />
           <TextInput
             autoComplete="tel"
             id="phone"
-            label="電話番号："
+            label="電話番号"
             name="phone"
-            placeholder="090-0000-0000"
+            placeholder="例）09012345678"
+            required
             type="tel"
           />
           <PasswordInput
             autoComplete="new-password"
             id="password"
-            label="パスワード："
+            label="パスワード"
             name="password"
+            placeholder="半角英数字を含む8文字以上"
+            required
           />
+          <p className="auth-form__note">
+            8文字以上の英数字を含むパスワードを設定してください
+          </p>
           <PasswordInput
             autoComplete="new-password"
             id="password-confirmation"
-            label="パスワード確認："
+            label="パスワード確認"
             name="passwordConfirmation"
+            placeholder="もう一度パスワードを入力してください"
+            required
           />
           <Button type="submit">登録する</Button>
         </form>
